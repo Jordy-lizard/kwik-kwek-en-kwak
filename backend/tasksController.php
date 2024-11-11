09f22eadb21548133b94f36d6a322eb99ce2706e
Jordy-lizard <d270021@edu.curio.nl>
2024-11-11 09:56:05 +0100

    taken wegschrijven naar database

--- backend/tasksController.php
+++ backend/tasksController.php
@@ -2,28 +2,49 @@
 
 $action = $_POST['action'];
 if($action == "create"){
-    $taak = $_POST['taak'];
-    if(empty($taak)){
-        $errors[]="Vul een taak in.";
-    }
     $naam = $_POST['naam'];
     if(empty($naam)){
         $errors[]="Vul een naam in.";
+    }
+    $titel = $_POST['titel'];
+    if(empty($titel)){
+        $errors[]="Vul een titel in.";
     }
     $beschrijving = $_POST['beschrijving'];
     if(empty($beschrijving)){
         $errors[]="Voer een beschrijving in.";
     }
+    $afdeling = isset($_POST['afdeling']) ? $_POST['afdeling'] : null;
+    if (empty($afdeling))
+    {
+        $errors[] = "Kies een afdeling.";
+    }
     
-}
+    //1. Verbinding
+    require_once '../backend/conn.php';
 
-if($action == "edit"){
+    //2. Query
+    $query = "INSERT INTO taken (titel, naam,beschrijving, afdeling) 
+    VALUES(:titel, :naam, :beschrijving, :afdeling)";
+
+    //3. Prepare
+    $statement = $conn->prepare($query);
     
+
+    //4. Execute
+    $statement->execute([
+        ":titel" => $titel,
+        ":afdeling" => $afdeling,
+        ":beschrijving" => $beschrijving,
+        ":naam" => $naam
+    ]);
+
+    header("Location: " . $base_url . "/tasks/index.php?msg=Taakopgeslagen");
+
 }
 
-if($action == "move"){
+if($action == "edit"){}
 
-}
 
 if($action == "delete"){
 
