6470c8ba10d0da38ca2e7fc0943c84ec9d183731
Jordy-lizard <d270021@edu.curio.nl>
2024-11-11 11:46:11 +0100

    taken controller gefixed

--- backend/tasksController.php
+++ backend/tasksController.php
@@ -1,52 +1,53 @@
 <?php
 
 $action = $_POST['action'];
-if($action == "create"){
+if ($action == "create") {
     $naam = $_POST['naam'];
-    if(empty($naam)){
-        $errors[]="Vul een naam in.";
+    if (empty($naam)) {
+        $errors[] = "Vul een naam in.";
     }
     $titel = $_POST['titel'];
-    if(empty($titel)){
-        $errors[]="Vul een titel in.";
+    if (empty($titel)) {
+        $errors[] = "Vul een titel in.";
     }
     $beschrijving = $_POST['beschrijving'];
-    if(empty($beschrijving)){
-        $errors[]="Voer een beschrijving in.";
+    if (empty($beschrijving)) {
+        $errors[] = "Voer een beschrijving in.";
     }
     $afdeling = isset($_POST['afdeling']) ? $_POST['afdeling'] : null;
-    if (empty($afdeling))
-    {
+    if (empty($afdeling)) {
         $errors[] = "Kies een afdeling.";
     }
-    
+
     //1. Verbinding
     require_once '../backend/conn.php';
 
     //2. Query
-    $query = "INSERT INTO taken (titel, naam,beschrijving, afdeling) 
-    VALUES(:titel, :naam, :beschrijving, :afdeling)";
+    $query = "INSERT INTO taken (naam, titel, beschrijving, afdeling) 
+    VALUES(:naam, :titel, :beschrijving, :afdeling)";
+
 
     //3. Prepare
     $statement = $conn->prepare($query);
-    
+
 
     //4. Execute
     $statement->execute([
+        ":naam" => $naam,
         ":titel" => $titel,
-        ":afdeling" => $afdeling,
         ":beschrijving" => $beschrijving,
-        ":naam" => $naam
+        ":afdeling" => $afdeling
     ]);
 
     header("Location: " . $base_url . "/tasks/index.php?msg=Taakopgeslagen");
 
 }
 
-if($action == "edit"){}
+if ($action == "edit") {
+}
 
 
-if($action == "delete"){
+if ($action == "delete") {
 
 }
 
--- tasks/create.php
+++ tasks/create.php
@@ -1,43 +1,51 @@
 <?php require_once '../head.php' ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php require_once "../head.php" ?>
 </head>
-<body>
-    
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
 <body>
 
-<form action="create.php" method="post">
-        <label for="naam">Naam:</label>
-        <input type="text" id="naam" name="naam">
+    <html lang="en">
 
-        <label for="titel">Titel:</label>
-        <input type="text" id="titel" name="titel">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-        <label for="beschrijving">Beschrijving:</label>
-        <textarea id="beschrijving" name="beschrijving" rows="4" cols="50"></textarea>
+    <body>
 
-        <label for="afdeling">Afdeling:</label>
-        <select id="afdeling" name="afdeling">
-            <option value="personeel">personeel</option>
-            <option value="horeca">horeca</option>
-            <option value="techniek">techniek</option>
-            <option value="inkoop">inkoop</option>
-            <option value="klantenservice">klantenservice</option>
-            <option value="groen">groen</option>
-        </select>
+        <form action="<?php echo $base_url; ?>/backend/tasksController.php" method="post">
+        <input type= "hidden" name= "action"value="create">
 
-        <button type="submit">Verstuur</button>
-    </form>
-    
-</body>
-</html>
+            <label for="naam">Naam:</label>
+            <input type="text" id="naam" name="naam">
+
+            <label for="titel">Titel:</label>
+            <input type="text" id="titel" name="titel">
+
+            <label for="beschrijving">Beschrijving:</label>
+            <textarea id="beschrijving" name="beschrijving" rows="4" cols="50"></textarea>
+
+            <label for="afdeling">Afdeling:</label>
+            <select id="afdeling" name="afdeling">
+                <option value="personeel">personeel</option>
+                <option value="horeca">horeca</option>
+                <option value="techniek">techniek</option>
+                <option value="inkoop">inkoop</option>
+                <option value="klantenservice">klantenservice</option>
+                <option value="groen">groen</option>
+            </select>
+
+            <button type="submit">Verstuur</button>
+        </form>
+
+    </body>
+
+    </html>
