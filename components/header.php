359a57a727b37a0fb70a1e0f5e5a45790e09261b
LilithParkerr <drp0016@edu.curio.nl>
2024-11-22 10:14:46 +0100

    better styling header

--- components/header.php
+++ components/header.php
@@ -2,11 +2,14 @@
 <?php require_once __DIR__ . "/../backend/config.php"; ?>
 
 <header>
+    <div class="logoHdr">
+<img src="../img/logo-big-v4.png" alt="sorryyy ik heb je img opgegeten" height="50px" width="175px">
+    </div>
 
     <div class="container">
         <nav>
-            <a href="<?php echo $base_url; ?>/index.php">Home</a> |
-            <a href="<?php echo $base_url; ?>/tasks/taken.php">taken</a>|
+            <a href="<?php echo $base_url; ?>/index.php">Home</a> 
+            <a href="<?php echo $base_url; ?>/tasks/taken.php">taken</a>
             <a href="<?php echo $base_url; ?>/tasks/done.php">done</a>
         </nav>
         
