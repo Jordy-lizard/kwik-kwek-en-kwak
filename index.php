36682f6b3e19b898c604234f6057586a788306b5
LilithParkerr <drp0016@edu.curio.nl>
2024-11-27 11:25:02 +0100

    basic styling index + css page index added

--- index.php
+++ index.php
@@ -5,17 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/index.css">
 </head>
+<header>
+
+<?php require_once "components/header.php" ?>
+</header>
 
 <body>
-
-    <?php require_once "components/header.php" ?>
-    <a href="tasks/create.php">Form</a>
+<div class="box1">
+ <div class="item1"><a href="tasks/create.php">Ga naar form</a></div>
 
     <?php if (isset($_GET['msg'])) {
         echo "<div class='msg'>" . $_GET['msg'] . "</div>";
     } ?>
-    <p>test</p>
+   <div class="item2"><p>welkom op onze homepage!</p></div> 
+    </div>
 </body>
 
 </html>
